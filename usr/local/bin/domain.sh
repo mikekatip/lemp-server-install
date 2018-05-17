240737a88a71527b7f3e09670508afe17573ced3
Mike Katip <mike@katip.name>
2018-05-17 09:45:34 -0400

    Update domain.sh

--- usr/local/bin/domain.sh
+++ usr/local/bin/domain.sh
@@ -5,25 +5,21 @@ WEBROOT="/var/www"
 USAGE="\nUsage: $(basename -- "$0") [COMMAND] domain.tld\n\nCommands:\n\n\tadd\n\tremove\n"
 
 if [ "$(whoami)" != "root" ]; then
-
     echo ERROR: Insufficient permissions to use this command.
-
 else
-
-	if [ ! -z "$1" ] && [ ! -z "$2" ]; then
-		if [ $1 == "add" ]; then
-			echo "Adding $2..."
-
+    if [ ! -z "$1" ] && [ ! -z "$2" ]; then
+	   if [ $1 == "add" ]; then
+            
+            echo "Adding $2..."
+            
             sudo bash -c "cat << 'EOF' > /etc/nginx/conf.d/$2.conf
 server {
     listen       80;
     server_name  $2 www.$2;
-
     location / {
         root   $WEBROOT/$2;
         index  index.php index.html index.htm;
     }
-
   location ~ \.php$ {
     fastcgi_index index.php;
     fastcgi_keep_conn on;
@@ -32,14 +28,12 @@ server {
     fastcgi_param SCRIPT_FILENAME $WEBROOT/$2\$fastcgi_script_name;
   }
 }
-
 EOF"
             sudo mkdir -p $WEBROOT/$2            
 
 			sudo bash -c "cat << 'EOF' > $WEBROOT/$2/info.php
 <?php phpinfo(); ?>
 EOF"
-
 
 			sudo bash -c "cat << 'EOF' > $WEBROOT/$2/index.php
 <!DOCTYPE html>
@@ -80,27 +74,26 @@ EOF"
 </html>
 EOF"
 
-		sudo chown -R www-data:sudo $WEBROOT/$2
-		sudo chmod -R 775 $WEBROOT/$2
+            sudo chown -R www-data:sudo $WEBROOT/$2
+            sudo chmod -R 775 $WEBROOT/$2
 	       	sudo chmod -R g+s $WEBROOT/$2
 	       	sudo chmod -R o-rwx $WEBROOT/$2
 	       	sudo chmod -R 775 $WEBROOT/$2
-		# sudo chown -R "$USER":www-data $WEBROOT/$2
-		# sudo chmod -R 0755 $WEBROOT/$2
+            # sudo chown -R "$USER":www-data $WEBROOT/$2
+            # sudo chmod -R 0755 $WEBROOT/$2
 			
-            	sudo systemctl restart nginx
+            sudo systemctl restart nginx
             
             if [[ ${2} != *".local"* ]];then
             	sudo certbot certonly -a webroot --webroot-path=$WEBROOT/$2 -d $2 -d www.$2
-            
+                
                 sudo bash -c "cat << 'EOF' > /etc/nginx/conf.d/$2.conf
- server {
+server {
     listen 80;
     listen [::]:80;
     server_name $2 www.$2;
     return 301 https://$2\$request_uri;
  }
-
  server {
     listen 443 ssl;
     listen [::]:443;
@@ -110,7 +103,6 @@ EOF"
     ssl_certificate_key /etc/letsencrypt/live/$2/privkey.pem;
     include ssl-params.conf;
  }
-
  server {
     listen 443 ssl;
     listen [::]:443;
@@ -118,12 +110,10 @@ EOF"
     ssl_certificate /etc/letsencrypt/live/$2/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/$2/privkey.pem;
     include ssl-params.conf;
-
     location / {
         root   $WEBROOT/$2;
         index  index.php index.html index.htm;
     }
-
   location ~ \.php$ {
     fastcgi_index index.php;
     fastcgi_keep_conn on;
@@ -132,15 +122,12 @@ EOF"
     fastcgi_param SCRIPT_FILENAME $WEBROOT/$2\$fastcgi_script_name;
   }
  }
-
- EOF"
-
-            	sudo systemctl restart nginx               
-       	    else
+EOF"
+        	sudo systemctl restart nginx               
+   	    else
        		sudo bash -c "cat << 'EOF' >> /etc/hosts
 127.0.0.1 $2
 127.0.0.1 www.$2
-
 EOF"
             fi              
  		fi 
